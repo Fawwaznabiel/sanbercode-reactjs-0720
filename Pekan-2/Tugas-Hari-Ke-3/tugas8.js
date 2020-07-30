e51b02eab1c67889d90dea392ae5679344bd1baf
Fawwaznabiel <36037441+Fawwaznabiel@users.noreply.github.com>
2020-07-30 07:24:00 +0700

    Update tugas8.js

--- Pekan-2/Tugas-Hari-Ke-3/tugas8.js
+++ Pekan-2/Tugas-Hari-Ke-3/tugas8.js
@@ -10,7 +10,7 @@ console.log('Luas Lingkaran =',luasLingkaran(3.14,4));
 const kelilingLingkaran = function(phi,r){
     return 2*phi*r;
 }
-console.log("Keliling Lingkaran =",kelilingLingkaran(4,2))
+console.log("Keliling Lingkaran =",kelilingLingkaran(3.14,2))
 console.log()
 
 //SOAL NOMOR 2
@@ -27,24 +27,26 @@ console.log()
 //SOAL NOMOR 3
 console.log("--- Soal Nomor 3 ---")
 console.log()
+var name = "Kalkulus"
+var totalPage = 200
+var price = 300000
 class book {
     constructor(name, totalPage, price){
         this.name = name
         this.totalPage = totalPage
         this.price = price
-    }
-    
+    } 
 }
+var mapel = new book(name,totalPage,price)
+console.log(mapel)
+
+
 class comic extends book {
     constructor(name, totalPage, price){
         super (name, totalPage, price)
         this.isColorful = true
     }
-    show(){
-        return "Nama Buku: " + this.name + " "+ "Jumlah Halaman: "  +this.totalPage + " "+ "Harga: " +this.price
-    }
 }
-buku = new book("Kalkulus", 200, 100000)
-
-buku = new comic("Naruto", 10, 10000)
-console.log(buku.show())
+var buku = new comic("Naruto", 10, 10000)
+console.log(buku)
+console.log()
